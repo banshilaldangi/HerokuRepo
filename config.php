3226c96ab1cc7e2834d6710ad02c896a3b672d66
banshilaldangi <banshi@concret.io>
2018-06-20 18:23:50 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("CLIENT_ID", "3MVG9d8..z.hDcPJZrfStsa8RxYEQ819aHYC2zSv0wgh8v.Q.T_LDuvp.liYmV0qca61PHt_.zFbipBoZgypn");
+
+define("CLIENT_SECRET", "813846095515858603");
+
+define("REDIRECT_URI", "https://obscure-basin-7348.herokuapp.com/oauth_callback.php");
+
+define("LOGIN_URI", "https://login.salesforce.com");
+
+?>
